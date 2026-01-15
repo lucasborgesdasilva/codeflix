6cfbdac3854c464e085e30d38d9cdb12f479703d
Lucas Borges da Silva <lucasborges2812@gmail.com>
2026-01-14 22:05:17 -0300

    Add routing with react-router-dom, implement Home and About pages and create appTheme

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Box>
-      <Container maxWidth="lg" sx={{ mt: 4, mb: 4, backgroundColor: "#000" }}>
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4, color: "white" }}>
         {children}
       </Container>
     </Box>
