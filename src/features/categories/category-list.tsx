09f55f6979990e137d40f946aed80edfc2dc10d2
Lucas Borges da Silva <lucasborges2812@gmail.com>
2026-01-16 00:46:03 -0300

    Refactor CategoryList to enhance data display and add action buttons

--- src/features/categories/category-list.tsx
+++ src/features/categories/category-list.tsx
@@ -1,5 +1,6 @@
-import { Box, Button } from "@mui/material";
-import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
+import { Delete } from "@mui/icons-material";
+import { Box, Button, IconButton, Typography } from "@mui/material";
+import { DataGrid, GridColDef, GridRenderCellParams, GridRowsProp } from "@mui/x-data-grid";
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../../app/hooks";
 import { selectCategories } from "./category-slice";
@@ -13,16 +14,54 @@ export const CategoryList = () => {
   const rows: GridRowsProp = categories.map(category => ({
     id: category.id,
     name: category.name,
-    description: category.description,
-    is_active: category.is_active
+    is_active: category.is_active,
+    createdAt: new Date(category.created_at)
   }))
 
   const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 300 },
-    { field: 'name', headerName: 'Name', width: 100 },
-    { field: 'description', headerName: 'Description', width: 500 },
-    { field: 'is_active', headerName: 'Active?', width: 100 },
+    { field: 'name', headerName: 'Name', flex: 1 },
+    {
+      field: 'is_active',
+      headerName: 'Active?',
+      width: 150,
+      type: "boolean",
+      renderCell: renderIsActiveCell,
+    },
+    {
+      field: 'createdAt',
+      headerName: 'Created At',
+      flex: 1,
+      type: "date"
+    },
+    {
+      field: 'id',
+      headerName: 'Actions',
+      headerAlign: "center",
+      flex: 1,
+      align: "center",
+      renderCell: renderActionsCell,
+    },
   ];
+
+  function renderIsActiveCell(row: GridRenderCellParams) {
+    return (
+      <Typography color={row.value ? "success" : "secondary"}>
+        {row.value ? "Active" : "Inactive"}
+      </Typography>
+    );
+  }
+
+  function renderActionsCell(params: GridRenderCellParams) {
+    return (
+      <IconButton
+        color="secondary"
+        onClick={() => console.log(params.value)}
+        aria-label="delete"
+      >
+        <Delete />
+      </IconButton>
+    )
+  }
 
 
   return (
