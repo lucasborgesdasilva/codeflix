d9b5bf91f70e5457d29189cc6835231a2aaf1fd1
Lucas Borges da Silva <lucasborges2812@gmail.com>
2026-01-27 16:23:17 -0300

    Add getCastMembers query to castMembersApiSlice and implement query parameter parsing for improved API integration

--- src/features/cast-members/cast-members-slice.ts
+++ src/features/cast-members/cast-members-slice.ts
@@ -1,4 +1,4 @@
-import { CastMember, CastMemberParams } from "../../types/cast-members";
+import { CastMember, CastMemberParams, Results } from "../../types/cast-members";
 import { apiSlice } from "../api/api-slice";
 
 const endpointUrl = "/cast_members";
@@ -13,12 +13,45 @@ export const initialState: CastMember = {
 };
 
 function parseQueryParams(params: CastMemberParams) {
-  return null;
+  const query = new URLSearchParams();
+
+  if (params.page) {
+    query.append("page", params.page.toString());
+  }
+  if (params.perPage) {
+    query.append("per_page", params.perPage.toString());
+  }
+  if (params.search) {
+    query.append("search", params.search);
+  }
+  if (params.type) {
+    query.append("type", params.type.toString());
+  }
+
+  return query.toString();
 }
 
-export const castMembersApiSlice = apiSlice.injectEndpoints({
-  endpoints: ({ query, mutation }) => ({})
+function getCastMembers(params: CastMemberParams) {
+  const { page = 1, perPage = 10, search, type } = params;
+
+  return `${endpointUrl}?${parseQueryParams({
+    page,
+    perPage,
+    search,
+    type
+  })}`
+}
+
+const castMembersApiSlice = apiSlice.injectEndpoints({
+  endpoints: ({ query, mutation }) => ({
+    getCastMembers: query<Results, CastMemberParams>({
+      query: getCastMembers,
+      providesTags: ["CastMembers"]
+    })
+  })
 });
 
-export { };
+export const {
+  useGetCastMembersQuery
+} = castMembersApiSlice;
 
