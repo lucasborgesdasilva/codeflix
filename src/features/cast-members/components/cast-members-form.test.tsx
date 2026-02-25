d0140846c5bed821b37bb8c413de8a64f7b33f66
Lucas Borges da Silva <lucasborges2812@gmail.com>
2026-02-25 13:41:18 -0300

    add first snapshot test of cast members form

---
+++ src/features/cast-members/components/cast-members-form.test.tsx
@@ -0,0 +1,28 @@
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { CastMemberForm } from "./cast-members-form";
+
+const Props = {
+  castMember: {
+    id: "1",
+    name: "test",
+    type: 1,
+    deletedAt: null,
+    createdAt: "2026-02-25T00:00:00.000000Z",
+    updatedAt: "2026-02-25T00:00:00.000000Z",
+  },
+  isDisabled: false,
+  isLoading: false,
+  handleSubmit: jest.fn(),
+  handleChange: jest.fn(),
+}
+
+describe("CastMemberForm", () => {
+  it("should render castMember form correctly", () => {
+    const { asFragment } = render(<CastMemberForm {...Props} />, {
+      wrapper: BrowserRouter
+    });
+
+    expect(asFragment()).toMatchSnapshot();
+  })
+})
