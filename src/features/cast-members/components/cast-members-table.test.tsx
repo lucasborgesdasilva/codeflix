5c09a231349746c76d61183e41cef6761eac601c
Lucas Borges da Silva <lucasborges2812@gmail.com>
2026-02-25 22:39:01 -0300

    finish snapshot tests of CastMembersTable

--- src/features/cast-members/components/cast-members-table.test.tsx
+++ src/features/cast-members/components/cast-members-table.test.tsx
@@ -4,14 +4,40 @@ import { BrowserRouter } from "react-router-dom";
 import { CastMembersTable } from "./cast-members-table";
 
 const Props = {
-    data: undefined,
-    perPage: 20,
-    isFetching: false,
-    rowsPerPage: [10, 20, 30],
-    handleOnPageChange: (page: number) => {},
-    handleFilterChange: (filterModel: GridFilterModel) => {},
-    handleOnPageSizeChange: (perPage: number) => {},
-    handleDelete: (id: string) => {},
+  data: {
+    data: [
+          {
+            id: "1",
+            name: "teste",
+            type: 1,
+            deletedAt: null,
+            createdAt: "2026-02-25T00:00:00.000000Z",
+            updatedAt: "2026-02-25T00:00:00.000000Z",
+          },
+        ],
+        meta: {
+          currentPage: 1,
+          from: 1,
+          lastPage: 1,
+          path: "http://localhost:8000/api/cast_members",
+          perPage: 15,
+          to: 1,
+          total: 1,
+        },
+        links: {
+          first: "http://localhost:8000/api/cast_members?page=1",
+          last: "http://localhost:8000/api/cast_members?page=1",
+          prev: "",
+          next: "",
+        }
+  },
+  perPage: 20,
+  isFetching: false,
+  rowsPerPage: [10, 20, 30],
+  handleOnPageChange: (page: number) => {},
+  handleFilterChange: (filterModel: GridFilterModel) => {},
+  handleOnPageSizeChange: (perPage: number) => {},
+  handleDelete: (id: string) => {},
 }
 
 describe("CastMembersTable", () => {
@@ -31,52 +57,32 @@ describe("CastMembersTable", () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
-  it("should render castMember table with data", () => {
+  it("should render castMember table with empty data", () => {
     const { asFragment } = render(
       <CastMembersTable 
         {...Props} 
-        data={{
-          data: [
-            {
-              id: "1",
-              name: "teste",
-              type: 1,
-              deletedAt: null,
-              createdAt: "2026-02-25T00:00:00.000000Z",
-              updatedAt: "2026-02-25T00:00:00.000000Z",
-            },
-          ],
-          meta: {
-            currentPage: 1,
-            from: 1,
-            lastPage: 1,
-            path: "http://localhost:8000/api/cast_members",
-            perPage: 15,
-            to: 1,
-            total: 1,
-          },
-          links: {
-            first: "http://localhost:8000/api/cast_members?page=1",
-            last: "http://localhost:8000/api/cast_members?page=1",
-            prev: "",
-            next: "",
-          }
-        }} 
-      />, {
-        wrapper: BrowserRouter,
-      }
+        data={{ 
+          data: [], 
+          meta: {},
+        } as any}
+      />,
+      { wrapper: BrowserRouter, }
     );
 
     expect(asFragment()).toMatchSnapshot();
   });
 
-  it("should render castMember table with empty data", () => {
+    it("should render correct type", () => {
     const { asFragment } = render(
       <CastMembersTable 
         {...Props} 
-        data={{ data: [], meta: {} } as any} />, {
-        wrapper: BrowserRouter,
-      }
+        data={{ 
+          data: [{...Props.data.data[0], type: 2 }], 
+          links: { ...Props.data.links},
+          meta: { ...Props.data.meta},
+        }} 
+      />, 
+      { wrapper: BrowserRouter, }
     );
 
     expect(asFragment()).toMatchSnapshot();
