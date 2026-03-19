51c70b44847ce9d51e49c6bb72d76db16f77fe50
Lucas Borges da Silva <lucasborges2812@gmail.com>
2026-03-18 22:19:30 -0300

    add instegration tests of CategoryList Component

--- src/features/categories/category-list.test.tsx
+++ src/features/categories/category-list.test.tsx
@@ -1,9 +1,9 @@
 import { rest } from "msw";
 import { setupServer } from "msw/node";
-import { renderWithProviders, screen } from "../../utils/test-utils";
+import { renderWithProviders, screen, waitFor } from "../../utils/test-utils";
 import { baseUrl } from "../api/api-slice";
-import { categoryResponse } from "../cast/mocks";
 import { CategoryList } from "./category-list";
+import { categoryResponse } from "./mocks";
 
 export const handlers = [
   //Find all
@@ -24,10 +24,35 @@ describe("CategoryList", () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
-  it("should render loading state", async () => {
+  // it("should render loading state", async () => {
+  //   renderWithProviders(<CategoryList />);
+
+  //   const loading = await screen.findByRole("progressbar");
+  //   expect(loading).toBeInTheDocument();
+  // });
+
+  it("should render success state", async () => {
     renderWithProviders(<CategoryList />);
 
-    const loading = await screen.findByRole("progressbar");
-    expect(loading).toBeInTheDocument();
+    await waitFor(() => {
+      const name = screen.getByText("Orchid");
+
+      expect(name).toBeInTheDocument();
+    });
+  });
+
+  it("should render error state", async () => {
+    server.use(
+      rest.get(`${baseUrl}/categories`, (_, res, ctx) => {
+        return res(ctx.status(500));
+      }),
+    );
+
+    renderWithProviders(<CategoryList />);
+
+    await waitFor(() => {
+      const error = screen.getByText("Error fetching categories");
+      expect(error).toBeInTheDocument();
+    });
   });
 });
